0acbe8121fdd1ef4fda9594202e1cfce4dabf501
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2025-02-04 13:38:20 -0300

    add compatibility to any sqlite compatible types as the item to be bound on the query

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use sqlx::{
-    pool,
-    query::QueryAs,
-    query_as,
-    sqlite::{Sqlite, SqliteArguments, SqliteRow},
-    FromRow,
-};
+use sqlx::{query::QueryAs, sqlite, FromRow, SqlitePool};
 
 #[derive(Debug, sqlx::FromRow)]
 pub struct User {
@@ -18,43 +12,44 @@ impl User {
     }
 }
 
-fn bind_params<'a, T>(
-    query: QueryAs<'a, Sqlite, T, SqliteArguments<'a>>,
-    mut params: impl Iterator<Item = String>,
-) -> QueryAs<'a, Sqlite, T, SqliteArguments<'a>> {
-    match params.next() {
-        Some(param) => {
-            let new_query = query.bind(param);
-            bind_params(new_query, params)
-        }
-        None => query,
+fn bind_params<'a, T, P>(
+    query: QueryAs<'a, sqlx::Sqlite, T, sqlite::SqliteArguments<'a>>,
+    params: impl IntoIterator<Item = P>,
+) -> QueryAs<'a, sqlx::Sqlite, T, sqlite::SqliteArguments<'a>>
+where
+    T: for<'r> FromRow<'r, sqlite::SqliteRow>,
+    P: sqlx::Encode<'a, sqlx::Sqlite> + sqlx::Type<sqlx::Sqlite> + Send + 'a,
+{
+    let mut query = query;
+    for param in params {
+        query = query.bind(param);
     }
+    query
 }
 
-fn build_query_with_params<'a, T>(
+fn build_query_with_params<'a, T, P>(
     query: &'a str,
-    params: Vec<String>,
-) -> QueryAs<'a, Sqlite, T, SqliteArguments<'a>>
+    params: impl IntoIterator<Item = P>,
+) -> QueryAs<'a, sqlx::Sqlite, T, sqlite::SqliteArguments<'a>>
 where
-    T: for<'r> FromRow<'r, SqliteRow>,
+    T: for<'r> FromRow<'r, sqlite::SqliteRow>,
+    P: sqlx::Encode<'a, sqlite::Sqlite> + sqlx::Type<sqlite::Sqlite> + Send + 'a,
 {
-    bind_params(sqlx::query_as(query), params.into_iter())
+    let query = sqlx::query_as(query);
+    bind_params(query, params)
 }
 
 #[tokio::main]
 async fn main() {
-    let pool = sqlx::sqlite::SqlitePool::connect(":memory:").await.unwrap();
+    let pool = SqlitePool::connect(":memory:").await.unwrap();
     sqlx::query("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT)")
         .execute(&pool)
         .await
         .unwrap();
-    let user: User = build_query_with_params(
-        "INSERT INTO users (name) VALUES (?)",
-        vec!["Natan".to_string()],
-    )
-    .fetch_one(&pool)
-    .await
-    .unwrap();
+    let user: User = build_query_with_params("INSERT INTO users (name) VALUES (?)", ["Natan"])
+        .fetch_one(&pool)
+        .await
+        .unwrap();
     println!("{:?}", user);
     let users: Vec<User> = sqlx::query_as("SELECT * FROM users")
         .fetch_all(&pool)
